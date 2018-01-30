46c3e59728a8984de7fbdea15b594ddcf4cbec10
LeviDeuster <35726170+LeviDeuster@users.noreply.github.com>
2018-01-30 08:29:12 +0100

    Update PHP.php

--- goede-php/PHP.php
+++ goede-php/PHP.php
@@ -4,7 +4,16 @@
 
     $plane = "<div id='plane'> <img id='plane' src='plane.png'> </div>";
     $ufo = "<div id='plane'> <img id='plane' src='ufo.png'> </div>";
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="CSS.css">
+</head>
+<body>
+    <?php
     if ($tijd >= 6 && ($tijd < 12)){
         echo "Goede morgen, het is $tijd uur";
         echo "<div class='morning'></div>";
@@ -23,19 +32,8 @@
     elseif ($tijd >= 0 && ($tijd < 6)){
         echo "Goede nacht, het is $tijd uur";
         echo "<div class='night'></div>";
-        echo "<img id='plane' src='ufo.png'>";
         echo $ufo;
     }
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="CSS.css">
-</head>
-<body>
-
+    ?>
 </body>
 </html>
